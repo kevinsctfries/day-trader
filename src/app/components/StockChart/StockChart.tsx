2502314c9d14da6053726609a3c06efb8398ae33
Kevin Fries <kevinsctfries@gmail.com>
2025-07-09 19:59:48 -0400

    chore: fix overflow issue

--- src/app/components/StockChart/StockChart.tsx
+++ src/app/components/StockChart/StockChart.tsx
@@ -62,18 +62,33 @@ export default function StockChart({ stock, currentDay }: Props) {
         <ResponsiveContainer width="100%" height="100%">
           <LineChart
             data={chartData}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            margin={{ top: 5, right: 5, left: 5, bottom: 5 }}>
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis domain={["auto", "auto"]} />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={2}
+              tick={{ fontSize: 12 }}
+              interval="preserveStartEnd"
+            />
+            <YAxis
+              domain={["auto", "auto"]}
+              tickLine={false}
+              axisLine={false}
+              tickMargin={2}
+              tick={{ fontSize: 12 }}
+              width={40}
+            />
             <Tooltip />
             <Line
               type="linear"
               dataKey="price"
               stroke="#8884d8"
               isAnimationActive={false}
+              dot={false}
+              strokeWidth={2}
             />
-            isAnim
           </LineChart>
         </ResponsiveContainer>
       </div>
